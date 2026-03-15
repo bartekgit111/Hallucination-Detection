c538c71a9fdba58125f8b2930440d51c8b3cbe5a
Bartosz Chudek <bartosz.chudek@op.pl>
2026-03-15 21:53:08 +0100

    add vocabulary building

--- word2vec/data_sets.py
+++ word2vec/data_sets.py
@@ -1,10 +1,16 @@
 import re
+from collections import Counter
 
 class data():
-    def __init__(self,path):
+    def __init__(self, path):
         self.path = path
         self.raw_text = ""
         self.words = []
+        self.vocab = []
+        self.vocab_size = 0
+        self.word_to_index = {}
+        self.index_to_word = {}
+        self.word_counts = Counter()
 
     def raw_text_from_file(self):
         with open(self.path, "r", encoding="utf8") as file:
@@ -18,8 +24,31 @@ class data():
 
         self.words  = self.raw_text.split()
 
-wiki = data("data/enwiki8")
+    def build_vocab(self):
+        self.word_counts = Counter(self.words)
+        self.vocab = list(self.word_counts.keys())
+        self.vocab_size = len(self.vocab)
+
+        self.word_to_index = {
+            word: idx for idx, word in enumerate(self.vocab)
+        }
+
+        self.index_to_word = {
+            idx: word for idx, word in enumerate(self.vocab)
+        }
+
+
+
+wiki = data("data\enwik8")
 wiki.raw_text_from_file()
 wiki.tokenization()
+wiki.build_vocab()
 
-print(wiki.words[:20])
+print(len(wiki.words))
+print(wiki.words[:10])
+
+print(wiki.vocab_size)
+print(wiki.vocab[:10])
+
+print(list(wiki.word_to_index.items())[:10])
+print(list(wiki.index_to_word.items())[:10])
